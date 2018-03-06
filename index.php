bf44d2c4418bfb50a94f449b61445b73296bd562
panake <teerasak.pan@gmail.com>
2018-03-06 11:58:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 $content = file_get_contents('php://input');
-$url = 'https://richflag.com:8080/webhook'
-#$ch = curl_init($url);
-#curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-#$result = curl_exec($ch);			
-#curl_close($ch);			
-#echo $result . "";
+$url = 'https://richflag.com:8080/webhook';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+$result = curl_exec($ch);			
+curl_close($ch);			
+echo $result . "";
 echo "yyy"
