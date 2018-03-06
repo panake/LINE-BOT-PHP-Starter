3a7514daa2c52435ad01161a2c1ae0b9a6109df6
panake <teerasak.pan@gmail.com>
2018-03-06 12:24:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 if (!is_null($events['events'])) {
-  $url = 'http://richflag.com';
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-  $result = curl_exec($ch);			
-  curl_close($ch);			
-  echo $result . "";
-  echo "zzz";
+  foreach ($events['events'] as $event) {	
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+      $text = $event['message']['text'];	
+      $replyToken = $event['replyToken'];	
+      $messages = [				
+      'type' => 'text',				
+      'text' => $text			
+      ];
+      $url = 'http://richflag.com';
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+      $result = curl_exec($ch);			
+      curl_close($ch);			
+      echo $result . "";
+      echo "zzz";
+    }
+  }
 } else {
   echo "boo";
 }
