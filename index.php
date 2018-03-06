2aac52a0294e3ac858efaf913f33fa3dba82df5e
panake <teerasak.pan@gmail.com>
2018-03-06 12:21:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
-$url = 'http://richflag.com';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-$result = curl_exec($ch);			
-curl_close($ch);			
-echo $result . "";
-echo "zzz";
+if (!is_null($events['events'])) {
+  $url = 'http://richflag.com';
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+  $result = curl_exec($ch);			
+  curl_close($ch);			
+  echo $result . "";
+  echo "zzz";
+} else {
+  echo "boo";
+}
